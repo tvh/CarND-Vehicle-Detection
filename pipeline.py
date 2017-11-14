d5eee216d4ef04e4bedb7261ab15906a6a2340df
Timo von Holtz <vonholtz@cp-med.com>
2017-11-14 09:05:21 +0100

    Now it labels the images correctly

--- pipeline.py
+++ pipeline.py
@@ -7,6 +7,7 @@ import argparse
 from sklearn.externals import joblib
 from skimage.feature import hog
 import math
+from scipy.ndimage.measurements import label
 
 IMG_X = 1280
 IMG_Y = 720
@@ -22,15 +23,36 @@ def extract_features(img, x0, y0, x1, y1):
     res = np.concatenate([y_hog, u_hog, v_hog])
     return res
 
-def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
-    # Make a copy of the image
-    imcopy = np.copy(img)
-    # Iterate through the bounding boxes
-    for bbox in bboxes:
-        # Draw a rectangle given bbox coordinates
-        cv2.rectangle(imcopy, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, thick)
-    # Return the image copy with boxes drawn
-    return imcopy
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
 
 def annotate_image(clf, img):
     found_matches = []
@@ -46,12 +68,17 @@ def annotate_image(clf, img):
                 features = extract_features(img, x0, y0, x1, y1)
                 res = clf.predict([features])
                 if res:
-                    found_matches.append([x0,y0,x1,y1])
+                    found_matches.append([[x0,y0],[x1,y1]])
                 y0 = y0+s//2
             x0 = x0+s//4
             x1 = x0+s
-    img = draw_boxes(img, found_matches)
-    return img
+    heat = np.zeros_like(img[:,:,0]).astype(np.float)
+    add_heat(heat,found_matches)
+    heat = apply_threshold(heat,2)
+    heatmap = np.clip(heat, 0, 255)
+    labels = label(heatmap)
+    draw_img = draw_labeled_bboxes(np.copy(img), labels)
+    return draw_img
 
 def output_test_images():
     '''
